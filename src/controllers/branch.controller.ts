348e8efb2e426c14a6c073eb4b225aaed0de3f66
melvinantonioo <melvin.antonio88@gmail.com>
2024-11-01 01:16:13 +0700

    feat:prisma

--- src/controllers/branch.controller.ts
+++ src/controllers/branch.controller.ts
@@ -40,6 +40,76 @@ async function GetBranches(req: Request, res: Response, next: NextFunction) {
     }
 }
 
+//get name clauses contain / like 
+async function GetBranchesName(req: Request, res: Response, next: NextFunction) {
+    try {
+        interface IFilter {
+            name?: string
+        };
+
+        const { name } = req.query;
+
+        const filter: IFilter = {};
+
+        if (name) {
+            filter.name = name as string;
+        };
+
+        const data = await prisma.branch.findMany({
+            where: {
+                name: filter.name,
+            },
+        });
+
+        res.status(200).send({
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+//
+async function Pagination(req: Request, res: Response, next: NextFunction) {
+    try {
+        //pagination mainin skip an play 
+        interface IFilter {
+            page: number;
+            pageSize: number;
+        };
+
+        const { page, pageSize } = req.query;
+
+        const filter: IFilter = {
+            page: parseInt(page as string),
+            pageSize: parseInt(page as string)
+        };
+
+        //Skip And Take 
+        const data = await prisma.branch.findMany({
+            skip: filter.page != 1 ? (filter.page - 1) * filter.pageSize : 0,
+            take: filter.pageSize, //sampe sini untuk pagination 
+            where: { // membuat kondisi untuk terpenuhi 
+                AND: [
+                    {
+                        name: 'sby',
+                    },
+                    {
+                        location: 'surabaya',
+                    },
+                ],
+            },
+        });
+
+        res.status(200).send({
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
 async function GetBranchesByid(req: Request, res: Response, next: NextFunction) {
     try {
         const { id } = req.params
@@ -58,4 +128,121 @@ async function GetBranchesByid(req: Request, res: Response, next: NextFunction) 
     }
 }
 
-export { CreateBranch, GetBranches, GetBranchesByid };
+//updat branch
+async function UpdatBranchById(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { id } = req.params;
+        const { name, location } = req.body;
+        const data = await prisma.branch.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name,
+                location,
+            }
+        })
+
+        res.status(200).send({
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function DeleteBranch(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { id } = req.params
+        await prisma.branch.delete({
+            where: {
+                id: parseInt(id),
+            }
+        });
+
+        res.status(200).send({
+            message: "Success",
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+//untuk data relasi 
+async function JoinBranch(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { id } = req.params
+        const data = await prisma.branch.findUnique({
+            where: {
+                id: parseInt(id),
+            },
+            include: {
+                Manager: true,
+            },
+        });
+
+        res.status(200).send({
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+//ketika kita buat branch , maka manager akan terbuat : Transaction 
+// untuk memproteksi multiple insert , agar data tidak rusak 
+async function CreateBranchTransaction(req: Request, res: Response, next: NextFunction) {
+    try {
+        //syntax .create() -> insert di sql ({isinya field dari table yang kita buat di db})
+        const { name, location, managerName } = req.body
+
+        await prisma.$transaction(async (prisma) => {
+            const findBranch = await prisma.branch.findFirst({
+                where: {
+                    name,
+                }
+            })
+
+            if (findBranch) {
+                throw new Error("Branch with that name already exist")
+            }
+
+            const data = await prisma.branch.create({
+                data: {
+                    name,
+                    location,
+                },
+            });
+
+            const manager = await prisma.manager.create({
+                data: {
+                    name: managerName,
+                    branchId: data.id,
+                }
+            })
+
+            res.status(200).send({
+                message: "Success",
+                data,
+            })
+        })
+
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {
+    CreateBranch,
+    GetBranches,
+    GetBranchesByid,
+    GetBranchesName,
+    UpdatBranchById,
+    DeleteBranch,
+    CreateBranchTransaction,
+    JoinBranch,
+    Pagination
+};
--- src/routes/branch.routes.ts
+++ src/routes/branch.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { CreateBranch, GetBranches, GetBranchesByid } from "../controllers/branch.controller";
+import { CreateBranch, GetBranches, GetBranchesByid, GetBranchesName, UpdatBranchById, DeleteBranch } from "../controllers/branch.controller";
 
 const router = Router();
 
@@ -8,5 +8,11 @@ router.post("/branches", CreateBranch);
 
 router.get("/branches", GetBranches);
 router.get("/branches/:id", GetBranchesByid);
+
+router.get("/branches", GetBranchesName);
+
+router.patch("branches/:id", UpdatBranchById);
+
+router.delete("branches/:id", DeleteBranch);
 
 export default router;
